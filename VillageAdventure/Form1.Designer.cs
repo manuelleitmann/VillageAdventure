eb80d334863dd0357336778cf6aea1c193c97a9c
Alexander Promebner <alexander.promebner@hak-kitz.ac.at>
2021-10-07 08:32:05 +0200

    frm_login erstellt

--- VillageAdventure/Form1.Designer.cs
+++ VillageAdventure/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace VillageAdventure
 {
-    partial class Form1
+    partial class frm_login
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -29,13 +29,127 @@ namespace VillageAdventure
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_login));
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.lbl_header = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.pbx_logo = new System.Windows.Forms.PictureBox();
+            this.btn_newaccount = new System.Windows.Forms.Button();
+            this.btn_login = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_username.Location = new System.Drawing.Point(12, 109);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(98, 18);
+            this.lbl_username.TabIndex = 0;
+            this.lbl_username.Text = "Username";
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_password.Location = new System.Drawing.Point(12, 170);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(95, 18);
+            this.lbl_password.TabIndex = 1;
+            this.lbl_password.Text = "Password";
+            // 
+            // lbl_header
+            // 
+            this.lbl_header.AutoSize = true;
+            this.lbl_header.Font = new System.Drawing.Font("Verdana", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_header.Location = new System.Drawing.Point(156, 9);
+            this.lbl_header.Name = "lbl_header";
+            this.lbl_header.Size = new System.Drawing.Size(115, 38);
+            this.lbl_header.TabIndex = 2;
+            this.lbl_header.Text = "Login";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.Location = new System.Drawing.Point(129, 106);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(198, 27);
+            this.txt_username.TabIndex = 3;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_password.Location = new System.Drawing.Point(129, 167);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(198, 27);
+            this.txt_password.TabIndex = 4;
+            // 
+            // pbx_logo
+            // 
+            this.pbx_logo.Image = ((System.Drawing.Image)(resources.GetObject("pbx_logo.Image")));
+            this.pbx_logo.Location = new System.Drawing.Point(9, 9);
+            this.pbx_logo.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.pbx_logo.Name = "pbx_logo";
+            this.pbx_logo.Size = new System.Drawing.Size(114, 55);
+            this.pbx_logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbx_logo.TabIndex = 5;
+            this.pbx_logo.TabStop = false;
+            // 
+            // btn_newaccount
+            // 
+            this.btn_newaccount.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_newaccount.Location = new System.Drawing.Point(15, 247);
+            this.btn_newaccount.Name = "btn_newaccount";
+            this.btn_newaccount.Size = new System.Drawing.Size(143, 51);
+            this.btn_newaccount.TabIndex = 6;
+            this.btn_newaccount.Text = "New Account";
+            this.btn_newaccount.UseVisualStyleBackColor = true;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(184, 247);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(143, 51);
+            this.btn_login.TabIndex = 7;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(366, 342);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.btn_newaccount);
+            this.Controls.Add(this.pbx_logo);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.lbl_header);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_username);
+            this.Name = "frm_login";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_username;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.Label lbl_header;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.PictureBox pbx_logo;
+        private System.Windows.Forms.Button btn_newaccount;
+        private System.Windows.Forms.Button btn_login;
     }
 }
 
