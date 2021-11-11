19b2179eac47bf8dcbcf9ccf45a8b4fde5c493c8
Lukas Bucher <lukas.bucher@hak-kitz.ac.at>
2021-11-11 08:56:41 +0100

    JaR-movement+collision detection platform

--- VillageAdventure/Form1.Designer.cs
+++ VillageAdventure/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace VillageAdventure
 {
-    partial class Form1
+    partial class frm_JumpAndRun
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -29,21 +29,61 @@ namespace VillageAdventure
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.pbx_player = new System.Windows.Forms.PictureBox();
+            this.tmr_move = new System.Windows.Forms.Timer(this.components);
+            this.pbx_platform = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_platform)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // pbx_player
+            // 
+            this.pbx_player.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbx_player.Location = new System.Drawing.Point(195, 268);
+            this.pbx_player.Name = "pbx_player";
+            this.pbx_player.Size = new System.Drawing.Size(67, 91);
+            this.pbx_player.TabIndex = 0;
+            this.pbx_player.TabStop = false;
+            // 
+            // tmr_move
+            // 
+            this.tmr_move.Enabled = true;
+            this.tmr_move.Interval = 20;
+            this.tmr_move.Tick += new System.EventHandler(this.tmr_move_Tick);
+            // 
+            // pbx_platform
+            // 
+            this.pbx_platform.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pbx_platform.Location = new System.Drawing.Point(-1, 534);
+            this.pbx_platform.Name = "pbx_platform";
+            this.pbx_platform.Size = new System.Drawing.Size(1077, 19);
+            this.pbx_platform.TabIndex = 1;
+            this.pbx_platform.TabStop = false;
+            // 
+            // frm_JumpAndRun
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(1076, 552);
+            this.Controls.Add(this.pbx_platform);
+            this.Controls.Add(this.pbx_player);
+            this.Name = "frm_JumpAndRun";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Load += new System.EventHandler(this.frm_JumpAndRun_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_platform)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbx_player;
+        private System.Windows.Forms.Timer tmr_move;
+        private System.Windows.Forms.PictureBox pbx_platform;
     }
 }
 
--- VillageAdventure/Form1.cs
+++ VillageAdventure/Form1.cs
@@ -10,15 +10,78 @@ using System.Windows.Forms;
 
 namespace VillageAdventure
 {
-    public partial class Form1 : Form
+    public partial class frm_JumpAndRun : Form
     {
-        public Form1()
+        #region variables
+        private bool left = false;
+        private bool right = false;
+        private bool jump = false;
+        private int gravity = 15;
+        private int force = 0;
+        private int speed = 10;
+        private bool playerOnFloor;
+        #endregion
+
+        public frm_JumpAndRun()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+
+        private void keyDown(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.A)
+            {
+                left = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                right = true;
+            }
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = gravity;
+                }
+            }
+        }
+
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            left = false;
+            right = false;
+        }
+
+        private void frm_JumpAndRun_Load(object sender, EventArgs e)
+        {
+            pbx_player.Location = new Point(0, ClientSize.Height - pbx_player.Height - pbx_platform.Height);
+            this.DoubleBuffered = true;//makes the movement of the player smoother
+            //set image
+        }
+
+        private void tmr_move_Tick(object sender, EventArgs e)
+        {
+
+            if (left && pbx_player.Left > 0)
+            {
+                pbx_player.Left -= speed;
+            }
+            if (right && pbx_player.Left + pbx_player.Width < ClientSize.Width)
+            {
+                pbx_player.Left += speed;
+            }
+
+            if (jump)
+            {
+                pbx_player.Top -= force;
+                force -= 2;
+            }
+            if (pbx_player.Bounds.IntersectsWith(pbx_platform.Bounds))
+            {
+                pbx_player.Top = ClientSize.Height - pbx_player.Height - pbx_platform.Height;                
+            }
 
         }
     }
