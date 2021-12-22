ab1841e3d923ff10c4e9593e14a982d95999f211
Manuel Leitmann <manuel.leitmann@hak-kitz.ac.at>
2021-12-22 11:05:32 +0100

    PacMan fist movement steps

--- VillageAdventure/PacMan.Designer.cs
+++ VillageAdventure/PacMan.Designer.cs
@@ -29,22 +29,39 @@ namespace VillageAdventure
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pbx_pacMan = new System.Windows.Forms.PictureBox();
             this.pbx_prison1 = new System.Windows.Forms.PictureBox();
             this.pbx_prison3 = new System.Windows.Forms.PictureBox();
             this.pbx_prison2 = new System.Windows.Forms.PictureBox();
             this.pbx_door = new System.Windows.Forms.PictureBox();
+            this.pbx_ghost1 = new System.Windows.Forms.PictureBox();
+            this.pbx_ghost2 = new System.Windows.Forms.PictureBox();
+            this.pbx_ghost3 = new System.Windows.Forms.PictureBox();
+            this.pbx_wall1 = new System.Windows.Forms.PictureBox();
+            this.pbx_wall2 = new System.Windows.Forms.PictureBox();
+            this.pbx_wall3 = new System.Windows.Forms.PictureBox();
+            this.pbx_wall4 = new System.Windows.Forms.PictureBox();
+            this.tmr_PacManLeft = new System.Windows.Forms.Timer(this.components);
+            this.tmr_PacManUp = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pbx_pacMan)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_prison1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_prison3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_prison2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_door)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_ghost1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_ghost2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_ghost3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wall1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wall2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wall3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wall4)).BeginInit();
             this.SuspendLayout();
             // 
             // pbx_pacMan
             // 
             this.pbx_pacMan.BackColor = System.Drawing.Color.Yellow;
-            this.pbx_pacMan.Location = new System.Drawing.Point(397, 397);
+            this.pbx_pacMan.Location = new System.Drawing.Point(602, 345);
             this.pbx_pacMan.Name = "pbx_pacMan";
             this.pbx_pacMan.Size = new System.Drawing.Size(55, 53);
             this.pbx_pacMan.TabIndex = 0;
@@ -53,16 +70,16 @@ namespace VillageAdventure
             // pbx_prison1
             // 
             this.pbx_prison1.BackColor = System.Drawing.Color.MidnightBlue;
-            this.pbx_prison1.Location = new System.Drawing.Point(299, 326);
+            this.pbx_prison1.Location = new System.Drawing.Point(343, 326);
             this.pbx_prison1.Name = "pbx_prison1";
-            this.pbx_prison1.Size = new System.Drawing.Size(260, 13);
+            this.pbx_prison1.Size = new System.Drawing.Size(175, 13);
             this.pbx_prison1.TabIndex = 1;
             this.pbx_prison1.TabStop = false;
             // 
             // pbx_prison3
             // 
             this.pbx_prison3.BackColor = System.Drawing.Color.MidnightBlue;
-            this.pbx_prison3.Location = new System.Drawing.Point(299, 209);
+            this.pbx_prison3.Location = new System.Drawing.Point(343, 209);
             this.pbx_prison3.Name = "pbx_prison3";
             this.pbx_prison3.Size = new System.Drawing.Size(15, 130);
             this.pbx_prison3.TabIndex = 2;
@@ -71,7 +88,7 @@ namespace VillageAdventure
             // pbx_prison2
             // 
             this.pbx_prison2.BackColor = System.Drawing.Color.MidnightBlue;
-            this.pbx_prison2.Location = new System.Drawing.Point(544, 209);
+            this.pbx_prison2.Location = new System.Drawing.Point(503, 209);
             this.pbx_prison2.Name = "pbx_prison2";
             this.pbx_prison2.Size = new System.Drawing.Size(15, 130);
             this.pbx_prison2.TabIndex = 3;
@@ -80,11 +97,82 @@ namespace VillageAdventure
             // pbx_door
             // 
             this.pbx_door.BackColor = System.Drawing.Color.Bisque;
-            this.pbx_door.Location = new System.Drawing.Point(299, 209);
+            this.pbx_door.Location = new System.Drawing.Point(343, 209);
             this.pbx_door.Name = "pbx_door";
-            this.pbx_door.Size = new System.Drawing.Size(260, 13);
+            this.pbx_door.Size = new System.Drawing.Size(175, 13);
             this.pbx_door.TabIndex = 4;
             this.pbx_door.TabStop = false;
+            // 
+            // pbx_ghost1
+            // 
+            this.pbx_ghost1.BackColor = System.Drawing.Color.Chartreuse;
+            this.pbx_ghost1.Location = new System.Drawing.Point(375, 244);
+            this.pbx_ghost1.Name = "pbx_ghost1";
+            this.pbx_ghost1.Size = new System.Drawing.Size(33, 38);
+            this.pbx_ghost1.TabIndex = 5;
+            this.pbx_ghost1.TabStop = false;
+            // 
+            // pbx_ghost2
+            // 
+            this.pbx_ghost2.BackColor = System.Drawing.Color.Fuchsia;
+            this.pbx_ghost2.Location = new System.Drawing.Point(419, 282);
+            this.pbx_ghost2.Name = "pbx_ghost2";
+            this.pbx_ghost2.Size = new System.Drawing.Size(33, 38);
+            this.pbx_ghost2.TabIndex = 6;
+            this.pbx_ghost2.TabStop = false;
+            // 
+            // pbx_ghost3
+            // 
+            this.pbx_ghost3.BackColor = System.Drawing.Color.Aqua;
+            this.pbx_ghost3.Location = new System.Drawing.Point(445, 228);
+            this.pbx_ghost3.Name = "pbx_ghost3";
+            this.pbx_ghost3.Size = new System.Drawing.Size(33, 38);
+            this.pbx_ghost3.TabIndex = 7;
+            this.pbx_ghost3.TabStop = false;
+            // 
+            // pbx_wall1
+            // 
+            this.pbx_wall1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pbx_wall1.Location = new System.Drawing.Point(503, 326);
+            this.pbx_wall1.Name = "pbx_wall1";
+            this.pbx_wall1.Size = new System.Drawing.Size(175, 13);
+            this.pbx_wall1.TabIndex = 8;
+            this.pbx_wall1.TabStop = false;
+            // 
+            // pbx_wall2
+            // 
+            this.pbx_wall2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pbx_wall2.Location = new System.Drawing.Point(274, 404);
+            this.pbx_wall2.Name = "pbx_wall2";
+            this.pbx_wall2.Size = new System.Drawing.Size(404, 10);
+            this.pbx_wall2.TabIndex = 9;
+            this.pbx_wall2.TabStop = false;
+            // 
+            // pbx_wall3
+            // 
+            this.pbx_wall3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pbx_wall3.Location = new System.Drawing.Point(663, 326);
+            this.pbx_wall3.Name = "pbx_wall3";
+            this.pbx_wall3.Size = new System.Drawing.Size(15, 88);
+            this.pbx_wall3.TabIndex = 10;
+            this.pbx_wall3.TabStop = false;
+            // 
+            // pbx_wall4
+            // 
+            this.pbx_wall4.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pbx_wall4.Location = new System.Drawing.Point(261, 117);
+            this.pbx_wall4.Name = "pbx_wall4";
+            this.pbx_wall4.Size = new System.Drawing.Size(15, 297);
+            this.pbx_wall4.TabIndex = 11;
+            this.pbx_wall4.TabStop = false;
+            // 
+            // tmr_PacManLeft
+            // 
+            this.tmr_PacManLeft.Tick += new System.EventHandler(this.tmr_PacManLeft_Tick);
+            // 
+            // tmr_PacManUp
+            // 
+            this.tmr_PacManUp.Tick += new System.EventHandler(this.tmr_PacManUp_Tick);
             // 
             // PacMan
             // 
@@ -92,6 +180,13 @@ namespace VillageAdventure
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(870, 658);
+            this.Controls.Add(this.pbx_wall4);
+            this.Controls.Add(this.pbx_wall3);
+            this.Controls.Add(this.pbx_wall2);
+            this.Controls.Add(this.pbx_wall1);
+            this.Controls.Add(this.pbx_ghost3);
+            this.Controls.Add(this.pbx_ghost2);
+            this.Controls.Add(this.pbx_ghost1);
             this.Controls.Add(this.pbx_door);
             this.Controls.Add(this.pbx_prison2);
             this.Controls.Add(this.pbx_prison3);
@@ -107,6 +202,13 @@ namespace VillageAdventure
             ((System.ComponentModel.ISupportInitialize)(this.pbx_prison3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_prison2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_door)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_ghost1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_ghost2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_ghost3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wall1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wall2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wall3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wall4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -118,5 +220,14 @@ namespace VillageAdventure
         private System.Windows.Forms.PictureBox pbx_prison3;
         private System.Windows.Forms.PictureBox pbx_prison2;
         private System.Windows.Forms.PictureBox pbx_door;
+        private System.Windows.Forms.PictureBox pbx_ghost1;
+        private System.Windows.Forms.PictureBox pbx_ghost2;
+        private System.Windows.Forms.PictureBox pbx_ghost3;
+        private System.Windows.Forms.PictureBox pbx_wall1;
+        private System.Windows.Forms.PictureBox pbx_wall2;
+        private System.Windows.Forms.PictureBox pbx_wall3;
+        private System.Windows.Forms.PictureBox pbx_wall4;
+        private System.Windows.Forms.Timer tmr_PacManLeft;
+        private System.Windows.Forms.Timer tmr_PacManUp;
     }
 }
--- VillageAdventure/PacMan.cs
+++ VillageAdventure/PacMan.cs
@@ -12,6 +12,9 @@ namespace VillageAdventure
 {
     public partial class PacMan : Form
     {
+        public bool checkLeft = false;
+        CharacterMovement movePacMan = new CharacterMovement();
+
         public PacMan()
         {
             InitializeComponent();
@@ -20,37 +23,34 @@ namespace VillageAdventure
 
         private void PacMan_Load(object sender, EventArgs e)
         {
-
+           
         }
 
         private void PacMan_KeyDown(object sender, KeyEventArgs e)
         {
-            CharacterMovement movePacMan = new CharacterMovement();
+            
             movePacMan.x = pbx_pacMan.Location.X;
             movePacMan.y = pbx_pacMan.Location.Y;
 
 
-            if (e.KeyCode == Keys.W)
+            if (e.KeyCode == Keys.W && checkLeft == true)
             {
-                movePacMan.MoveUp();
-                //to refresh the location of the picture box
-                pbx_pacMan.Location = new Point(movePacMan.x, movePacMan.y);
+                tmr_PacManUp.Enabled = true;
             }
             else if (e.KeyCode == Keys.S)
             {
-                movePacMan.MoveDown();
+                //movePacMan.MoveDown();
                 pbx_pacMan.Location = new Point(movePacMan.x, movePacMan.y);
-               
+
             }
-            else if (e.KeyCode == Keys.A)
+            else if (e.KeyCode == Keys.A && checkLeft == false)
             {
-                movePacMan.MoveLeft();
-                pbx_pacMan.Location = new Point(movePacMan.x, movePacMan.y);             
+                tmr_PacManLeft.Enabled = true;
             }
             else if (e.KeyCode == Keys.D)
             {
-                movePacMan.MoveRight();
-                pbx_pacMan.Location = new Point(movePacMan.x, movePacMan.y);
+                //movePacMan.MoveRight();
+                //pbx_pacMan.Location = new Point(movePacMan.x, movePacMan.y);
             }
             else if (e.KeyCode == Keys.Escape)
             {
@@ -58,6 +58,41 @@ namespace VillageAdventure
                 this.Hide();
                 pacManMenu.ShowDialog();
                 this.Close();
+            }
+        }
+
+        private void tmr_PacManLeft_Tick(object sender, EventArgs e)
+        {
+            movePacMan.x = pbx_pacMan.Location.X;
+            movePacMan.y = pbx_pacMan.Location.Y;
+
+            movePacMan.MoveLeft();
+            pbx_pacMan.Location = new Point(movePacMan.x, movePacMan.y);
+
+            if (pbx_pacMan.Bounds.IntersectsWith(pbx_wall4.Bounds))
+            {
+                tmr_PacManLeft.Enabled = false;
+                checkLeft = true;
+            }
+        }
+
+        private void Reset(int test)
+        {
+            //to reset everything in starting position after the user collected all coins
+        }
+
+        private void tmr_PacManUp_Tick(object sender, EventArgs e)
+        {
+            movePacMan.x = pbx_pacMan.Location.X;
+            movePacMan.y = pbx_pacMan.Location.Y;
+
+            movePacMan.MoveUp();
+            pbx_pacMan.Location = new Point(movePacMan.x, movePacMan.y);
+
+            if (pbx_pacMan.Bounds.IntersectsWith(pbx_wall4.Bounds))
+            {
+                tmr_PacManLeft.Enabled = false;
+                checkLeft = true;
             }
         }
     }
