795d81529c7ac0f5c0785655377a7c738f7c3eb5
Alexander Promebner <alexander.promebner@hak-kitz.ac.at>
2022-01-13 09:03:16 +0100

    Gravity changed and Bounds updated

--- VillageAdventure/DoodleJump.Designer.cs
+++ VillageAdventure/DoodleJump.Designer.cs
@@ -34,7 +34,12 @@ namespace VillageAdventure
             this.pbx_character = new System.Windows.Forms.PictureBox();
             this.tmr_falldown = new System.Windows.Forms.Timer(this.components);
             this.tmr_checkbounds = new System.Windows.Forms.Timer(this.components);
+            this.pbx_down = new System.Windows.Forms.PictureBox();
+            this.pbx_left = new System.Windows.Forms.PictureBox();
+            this.tmr_spawnplatform = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pbx_character)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_down)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_left)).BeginInit();
             this.SuspendLayout();
             // 
             // tmr_gravity
@@ -46,7 +51,7 @@ namespace VillageAdventure
             // pbx_character
             // 
             this.pbx_character.BackColor = System.Drawing.Color.Black;
-            this.pbx_character.Location = new System.Drawing.Point(139, 324);
+            this.pbx_character.Location = new System.Drawing.Point(133, 326);
             this.pbx_character.Name = "pbx_character";
             this.pbx_character.Size = new System.Drawing.Size(35, 29);
             this.pbx_character.TabIndex = 0;
@@ -65,12 +70,37 @@ namespace VillageAdventure
             this.tmr_checkbounds.Interval = 1;
             this.tmr_checkbounds.Tick += new System.EventHandler(this.tmr_checkbounds_Tick);
             // 
+            // pbx_down
+            // 
+            this.pbx_down.Location = new System.Drawing.Point(0, 460);
+            this.pbx_down.Name = "pbx_down";
+            this.pbx_down.Size = new System.Drawing.Size(330, 10);
+            this.pbx_down.TabIndex = 1;
+            this.pbx_down.TabStop = false;
+            this.pbx_down.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pbx_left
+            // 
+            this.pbx_left.Location = new System.Drawing.Point(-11, -1);
+            this.pbx_left.Name = "pbx_left";
+            this.pbx_left.Size = new System.Drawing.Size(10, 453);
+            this.pbx_left.TabIndex = 2;
+            this.pbx_left.TabStop = false;
+            // 
+            // tmr_spawnplatform
+            // 
+            this.tmr_spawnplatform.Enabled = true;
+            this.tmr_spawnplatform.Interval = 500;
+            this.tmr_spawnplatform.Tick += new System.EventHandler(this.tmr_spawnplatform_Tick);
+            // 
             // frm_doodlejump
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(329, 460);
+            this.Controls.Add(this.pbx_left);
+            this.Controls.Add(this.pbx_down);
             this.Controls.Add(this.pbx_character);
             this.Name = "frm_doodlejump";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -78,6 +108,8 @@ namespace VillageAdventure
             this.Load += new System.EventHandler(this.frm_doodlejump_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frm_doodlejump_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pbx_character)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_down)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_left)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -88,5 +120,8 @@ namespace VillageAdventure
         private System.Windows.Forms.Timer tmr_gravity;
         private System.Windows.Forms.Timer tmr_falldown;
         private System.Windows.Forms.Timer tmr_checkbounds;
+        private System.Windows.Forms.PictureBox pbx_down;
+        private System.Windows.Forms.PictureBox pbx_left;
+        private System.Windows.Forms.Timer tmr_spawnplatform;
     }
 }
--- VillageAdventure/DoodleJump.cs
+++ VillageAdventure/DoodleJump.cs
@@ -12,12 +12,13 @@ namespace VillageAdventure
 {
     public partial class frm_doodlejump : Form
     {
-        int gravity = 1;
-        
-        
-        int pbx_x, pbx_y, pbx_w, pbx_h;
+        #region Variablen
+        int gravity = 1;       
+        int pbx_y = 270;
         Random rand = new Random();
         List<PictureBox> items = new List<PictureBox>();
+        PictureBox pbx_test = new PictureBox();
+        #endregion
 
         public frm_doodlejump()
         {
@@ -35,6 +36,8 @@ namespace VillageAdventure
             CharacterMovement locations = new CharacterMovement();
             locations.x = pbx_character.Location.X;
             locations.y = pbx_character.Location.Y;
+
+            
 
             if (e.KeyCode == Keys.Right)
             {
@@ -56,46 +59,39 @@ namespace VillageAdventure
             {
                 pbx_character.Location = new Point(1, pbx_character.Location.Y);
             }
-            if (pbx_character.Left + pbx_character.Width < 1)
+            if (pbx_character.Bounds.IntersectsWith(pbx_left.Bounds))
             {
-                pbx_character.Location = new Point(400, pbx_character.Location.Y);
-            }
-
-
-            //try check with picture box
-            if (pbx_character.Top < 0 || pbx_character.Height + pbx_character.Top > this.ClientRectangle.Height)
-            {
-                MessageBox.Show("You Lost!");
-
-                gravity = 0;
-                pbx_character.Location = new Point(1, 231);
+                pbx_character.Location = new Point(288, pbx_character.Location.Y);
             }
         }
 
         private void frm_doodlejump_Load(object sender, EventArgs e)
         {
-            pbx_character.Location = new Point(136, 100);
-
             MakePlatform();
         }
 
-        PictureBox pbx_test = new PictureBox();
+        
 
         private void MakePlatform()
-        {            
+        {
+            
             pbx_test.Height = 30;
             pbx_test.Width = 55;
             pbx_test.SizeMode = PictureBoxSizeMode.Zoom;
             pbx_test.Image = Properties.Resources.DoodleJumpPlatformtransparent;
 
             int x = rand.Next(0, 275);
-            int y = rand.Next(10, 350);
+            int y = rand.Next(0, 350);
 
             pbx_test.Location = new Point(x, y);
 
             items.Add(pbx_test);
             this.Controls.Add(pbx_test);
+            
         }
+        
+        
+       
 
         private void tmr_falldown_Tick(object sender, EventArgs e)
         {
@@ -105,6 +101,16 @@ namespace VillageAdventure
         private void tmr_checkbounds_Tick(object sender, EventArgs e)
         {
             CheckBounds();
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmr_spawnplatform_Tick(object sender, EventArgs e)
+        {
+            //MakePlatform();
         }
 
         private void pbx_platform_Click(object sender, EventArgs e)
@@ -122,10 +128,31 @@ namespace VillageAdventure
 
         private void tmr_gravity_Tick(object sender, EventArgs e)
         {
-            if (pbx_character.Bounds.IntersectsWith(pbx_test.Bounds))
+            if (pbx_character.Bounds.IntersectsWith(pbx_test.Bounds) && pbx_test.Top + pbx_test.Height >= pbx_test.Top)
             {
                 gravity = -1;
             } 
+
+            if(pbx_character.Bounds.IntersectsWith(pbx_down.Bounds))
+            {
+                pbx_down.Location = new Point(800, 800);
+                DialogResult message = MessageBox.Show("You Lost!", "You was too slow to hit a platform!", MessageBoxButtons.YesNo);
+
+                if(message == DialogResult.Yes)
+                {
+                    frm_doodlejump dj = new frm_doodlejump();
+                    this.Hide();
+                    dj.ShowDialog();
+                    this.Close();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+
         }
     }
+
+    
 }
